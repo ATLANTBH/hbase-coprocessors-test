934998d91f5236b9268496fe75a39b758ff4bfc3
Davor Kovacic <vorda.k@gmail.com>
2012-02-10 14:15:57 +0100

    Added comments

--- src/main/java/com/abh/hbase/coprocessors/batchops/BatchOperationsProtocol.java
+++ src/main/java/com/abh/hbase/coprocessors/batchops/BatchOperationsProtocol.java
@@ -8,28 +8,31 @@ import org.apache.hadoop.hbase.ipc.CoprocessorProtocol;
 /**
  * Defines basic batch operations
  * 
- * Each method takes {@link BatchOperation} object as an argument
- * and returns {@link BatchOperationResult}
+ * Each method takes {@link BatchOperation} object as an argument and returns
+ * {@link BatchOperationResult}
  * 
  */
-public interface BatchOperationsProtocol extends CoprocessorProtocol{
+public interface BatchOperationsProtocol extends CoprocessorProtocol {
 
   /**
    * Updates all rows defined by {@link Scan} object in BatchOperation argument
    * with values defined in list of {@link BatchOperation.Value} objects
+   * 
    * @param update
    * @return
    * @throws IOException
    */
-	BatchOperationResult batchUpdate(BatchOperation update) throws IOException;
-	
-/**
- * Deletes from all rows defined by {@link Scan} object in BatchOperation argument.
- * Columns to delete are defined in list of {@link BatchOperation.Value} objects
- * @param delete
- * @return
- * @throws IOException
- */
-	BatchOperationResult batchDelete(BatchOperation delete) throws IOException;
+  BatchOperationResult batchUpdate(BatchOperation update) throws IOException;
+
+  /**
+   * Deletes from all rows defined by {@link Scan} object in BatchOperation
+   * argument. Columns to delete are defined in list of
+   * {@link BatchOperation.Value} objects
+   * 
+   * @param delete
+   * @return
+   * @throws IOException
+   */
+  BatchOperationResult batchDelete(BatchOperation delete) throws IOException;
 
 }
